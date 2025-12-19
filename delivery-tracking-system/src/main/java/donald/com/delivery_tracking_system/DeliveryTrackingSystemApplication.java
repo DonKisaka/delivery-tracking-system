cbd0f81cf9e6bd89ff29c818c3d1eb7d7e134692
DonaldKisaka <kisakadonald1@gmail.com>
2025-12-19 10:52:29 +0300

    Initial commit

---
+++ delivery-tracking-system/src/main/java/donald/com/delivery_tracking_system/DeliveryTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package donald.com.delivery_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryTrackingSystemApplication.class, args);
+	}
+
+}
